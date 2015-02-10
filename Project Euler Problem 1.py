76a3409411d66b3f7b41ad9c64c8dd6e88e2bbb0
Patrick Wasilewski <pwasilewski727@gmail.com>
2015-02-09 18:30:51 -0600

    Added problem 1

---
+++ Project Euler Problem 1.py
@@ -0,0 +1,22 @@
+x = 0
+y = 0
+z = 0
+sumx = 0
+sumy = 0
+sumz = 0
+total = 0
+
+while x < 999:
+    x = x + 3
+    sumx = sumx + x
+
+while y < 995:
+    y = y + 5
+    sumy = sumy + y
+
+while z < 985
+    z = z + 15
+    sumz = sumz + z
+
+total = sumx + sumy - sumz
+print(total)
